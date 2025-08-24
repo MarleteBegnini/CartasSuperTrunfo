a5c2012dc74dbebee57dbbb2c18a016cefd92d49
MarleteBegnini <marletebegnini83@gmail.com>
2025-08-24 22:30:10 +0000

    Inclusao de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,30 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Cadastro da Primeira Carta
+  // Cadastro da Carta 1
 
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado;
+    char Codigo[2];
+    char Cidade[20];
+    int Populacao;
+    float  Area;
+    float PIB;
+    int Numero_pontos_turisticos;
+  
+    printf("Digite o nome do Estado: \n");
+    scanf("%s",  &Estado);
+
+    printf("Digite o código da Carta: \n");
+    scanf("%s", &Codigo);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &Cidade);
+
+    printf("Digite o número de habitantes: \n");
+    scanf("%d", &Populacao);
+    
+  
 
   // Área para entrada de dados
 
