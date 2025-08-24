c5af2011ea95582ea621fb599c89554efc24c14b
MarleteBegnini <marletebegnini83@gmail.com>
2025-08-24 21:38:25 +0000

    Cadastro da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,8 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+  // Cadastro da Primeira Carta
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
